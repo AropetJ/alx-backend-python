70024903f55100e077ba51c415651a21b416654e
AropetJ <aropetjoel32@gmail.com>
2024-01-11 13:27:55 +0100

    Attempted task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''1-concat.py
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    """A typed-annotated function that concatenates 2 strings
+
+    Args:
+        str1 (str): string one
+        str2 (str): string two
+
+    Returns:
+        str: A concatenated string
+    """
+    return str1 + str2
