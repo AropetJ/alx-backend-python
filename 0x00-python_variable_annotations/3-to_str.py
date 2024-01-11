f51845025eb12fca64609563ac2f598d229d58eb
AropetJ <aropetjoel32@gmail.com>
2024-01-11 13:37:08 +0100

    Attempted task3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''3-to_str.py
+'''
+
+
+def to_str(n: float) -> str:
+    """Converts a float to a string
+
+    Args:
+        n (float): Float to covert to a string
+
+    Returns:
+        str: Float converted to a string
+    """
+    return str(n)
