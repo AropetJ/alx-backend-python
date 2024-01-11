578d99a44727b8bcdcf8bb9de83b5c9cf07723d8
AropetJ <aropetjoel32@gmail.com>
2024-01-11 13:32:45 +0100

    Attempted task2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''2-floor.py
+'''
+
+
+def floor(n: float) -> int:
+    """A typed anotation function that returns the floor of the float
+
+    Args:
+        n (float): The float whose floor is to be returned
+
+    Returns:
+        int: The returned floor
+    """
+    return int(n)
