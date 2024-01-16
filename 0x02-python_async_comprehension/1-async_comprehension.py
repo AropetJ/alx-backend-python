69a516e6b73c22afaa7c22064857a15ded9c6a86
AropetJ <aropetjoel32@gmail.com>
2024-01-16 10:23:47 +0100

    Attempted task 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 '''1-async_comprehension.py
 '''
+
+from typing import List
+from importlib import import_module as using
+
+
+async_generator = using('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Async function that returns a list of floats generated from an async
+    generator.
+    Returns:
+        List[float]: A list of floats generated from the async generator.
+    """
+    return [f async for f in async_generator()]
