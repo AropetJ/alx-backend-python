f6ac5a841178ca6612423e02dfc5f8a78a9726ce
AropetJ <aropetjoel32@gmail.com>
2024-02-06 00:09:09 +0100

    Attempted task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""A python module for testing the utils module."""
-
+"""A module for testing the utils module.
+"""
 import unittest
 from typing import Dict, Tuple, Union
 from unittest.mock import patch, Mock
@@ -15,13 +15,12 @@ from utils import (
 
 class TestAccessNestedMap(unittest.TestCase):
     """Tests the `access_nested_map` function."""
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map_returns_expected_value(
+    def test_access_nested_map(
             self,
             nested_map: Dict,
             path: Tuple[str],
@@ -34,7 +33,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
     ])
-    def test_access_nested_map_raises_exception(
+    def test_access_nested_map_exception(
             self,
             nested_map: Dict,
             path: Tuple[str],
@@ -47,12 +46,11 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     """Tests the `get_json` function."""
-
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    def test_get_json_returns_expected_payload(
+    def test_get_json(
             self,
             test_url: str,
             test_payload: Dict,
@@ -66,11 +64,8 @@ class TestGetJson(unittest.TestCase):
 
 class TestMemoize(unittest.TestCase):
     """Tests the `memoize` function."""
-
-    def test_memoize_caches_result(
-            self
-    ) -> None:
-        """Tests `memoize`'s caching behavior."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
         class TestClass:
             def a_method(self):
                 return 42
@@ -78,17 +73,12 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-
         with patch.object(
                 TestClass,
                 "a_method",
                 return_value=lambda: 42,
-        ) as memo_fxn:
+                ) as memo_fxn:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
             memo_fxn.assert_called_once()
-
-
-if __name__ == "__main__":
-    unittest.main()
