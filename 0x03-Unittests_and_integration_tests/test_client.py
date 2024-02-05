7fed024aaec8757c26d1b7604b43f724cfaf660d
AropetJ <aropetjoel32@gmail.com>
2024-02-06 00:47:21 +0100

    Attempted task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -12,21 +12,16 @@ from client import (GithubOrgClient)
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test case for the GithubOrgClient class."""
-    @parameterized.expand([
-        ("google", {'login': "google"}),
-        ("abc", {'login': "abc"}),
-    ])
-    @patch(
-        "client.get_json",
-    )
+    @parameterized.expand([("google", {'login': "google"}),
+                           ("abc", {'login': "abc"})])
+    @patch("client.get_json",)
     def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
         """Test the org method of GithubOrgClient."""
         mocked_fxn.return_value = MagicMock(return_value=resp)
         gh_org_client = GithubOrgClient(org)
         self.assertEqual(gh_org_client.org(), resp)
         mocked_fxn.assert_called_once_with(
-            "https://api.github.com/orgs/{}".format(org)
-        )
+            "https://api.github.com/orgs/{}".format(org))
 
     def test_public_repos_url(self) -> None:
         """Test the _public_repos_url property of GithubOrgClient."""
